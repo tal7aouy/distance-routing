ec1144e317bdab42bc5aff38cb31314ca69fc774
tal7aouy <talhaouycoding@gmail.com>
2021-07-16 22:09:38 +0100

    add longitude & latitude

--- src/App.js
+++ src/App.js
@@ -2,22 +2,46 @@ import { useEffect, useRef, useState } from 'react'
 import * as tt from '@tomtom-international/web-sdk-maps'
 function App() {
   const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(-3.7045)
+  const [latitude, setLatitude] = useState(33.339531)
   const mapElem = useRef()
   const API_KEY = 'V5DFo4X3uONIJmSWCvD24GobF5CCgGJ7'
-  const latitude = 34.029986
-  const longitude = -5.022786
+
   useEffect(() => {
     let myMap = tt.map({
       key: API_KEY,
       container: mapElem.current,
       center: [longitude, latitude],
       zoom: 14,
+      stylesVisibility: {
+        trafficFlow: true,
+        trafficIncidents: true,
+      },
     })
     setMap(myMap)
-  }, [])
+    // clean
+    return () => myMap.remove()
+  }, [longitude, latitude])
   return (
     <div className='container'>
-      <div ref={mapElem} className='map'></div>
+      <div className='search-bar'>
+        <h4>where to ?</h4>
+        <input
+          type='text'
+          id='latitude'
+          className='latitude'
+          placeholder='latitude'
+          onChange={(e) => setLatitude(e.target.value)}
+        />
+        <input
+          type='text'
+          id='longitude'
+          className='longitude'
+          placeholder='longitude'
+          onChange={(e) => setLongitude(e.target.value)}
+        />
+      </div>
+      <div ref={mapElem} className='map' />
     </div>
   )
 }
