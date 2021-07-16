016258acc31fb89160f06dfde75b8c6e6847827e
tal7aouy <talhaouycoding@gmail.com>
2021-07-16 22:30:16 +0100

    addMarker feature and dark it

--- src/App.js
+++ src/App.js
@@ -19,30 +19,46 @@ function App() {
       },
     })
     setMap(myMap)
-    // clean
+    // addMarker
+    const addMarker = () => {
+      const elem = document.createElement('div')
+      elem.className = 'marker'
+      const marker = new tt.Marker({
+        draggable: true,
+        element: elem,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(myMap)
+    }
+    addMarker()
+    // cleanup function
     return () => myMap.remove()
   }, [longitude, latitude])
   return (
-    <div className='container'>
-      <div className='search-bar'>
-        <h4>where to ?</h4>
-        <input
-          type='text'
-          id='latitude'
-          className='latitude'
-          placeholder='latitude'
-          onChange={(e) => setLatitude(e.target.value)}
-        />
-        <input
-          type='text'
-          id='longitude'
-          className='longitude'
-          placeholder='longitude'
-          onChange={(e) => setLongitude(e.target.value)}
-        />
-      </div>
-      <div ref={mapElem} className='map' />
-    </div>
+    <>
+      {map && (
+        <div className='container'>
+          <div className='search-bar'>
+            <h4>where to ?</h4>
+            <input
+              type='text'
+              id='latitude'
+              className='latitude'
+              placeholder='latitude'
+              onChange={(e) => setLatitude(e.target.value)}
+            />
+            <input
+              type='text'
+              id='longitude'
+              className='longitude'
+              placeholder='longitude'
+              onChange={(e) => setLongitude(e.target.value)}
+            />
+          </div>
+          <div ref={mapElem} className='map' />
+        </div>
+      )}
+    </>
   )
 }
 
